d1cf5a81fe2bef2fd116a8af4e31a86799105cc3
YangLOL <364762169@qq.com>
2018-05-10 22:03:28 +0800

    upadte
    AI ChatBot

---
+++ SEapp/app/src/main/java/com/example/zy5545269/seapp/Advisor.java
@@ -0,0 +1,17 @@
+package com.example.zy5545269.seapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Advisor {
+    public Advisor(){
+
+    }
+
+    public static List<String> getData()
+    {
+        List<String> list = new ArrayList<String>();
+        list.add("Advisor：" + "Nice to meet you！");
+        return list;
+    }
+}

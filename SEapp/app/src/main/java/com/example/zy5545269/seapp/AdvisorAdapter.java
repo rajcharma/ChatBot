886c7bd8e671ab6d3f4b48e40de4f106434b1f3c
YangLOL <364762169@qq.com>
2018-05-21 07:26:06 +0800

    bubble

--- SEapp/app/src/main/java/com/example/zy5545269/seapp/AdvisorAdapter.java
+++ SEapp/app/src/main/java/com/example/zy5545269/seapp/AdvisorAdapter.java
@@ -1,9 +1,11 @@
 package com.example.zy5545269.seapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,8 +19,10 @@ public class AdvisorAdapter extends ArrayAdapter<Advisor> {
         this.resourceId = textViewResourceId;
     }
 
-    class ViewValue()
+    class ViewValue
     {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
         TextView leftAdvisor;
         TextView RightUser;
     }
@@ -28,6 +32,33 @@ public class AdvisorAdapter extends ArrayAdapter<Advisor> {
         View view;
         ViewValue viewvalue;
 
+        if(convertView == null)
+        {
+            view = LayoutInflater.from(getContext()).inflate(resourceId,null);
+            viewvalue = new ViewValue();
+            viewvalue.rightLayout= (LinearLayout)view.findViewById(R.id.right_layout);
+            viewvalue.leftLayout=(LinearLayout)view.findViewById(R.id.left_layout);
+            viewvalue.leftAdvisor =(TextView)view.findViewById(R.id.tv_receive);
+            viewvalue.RightUser = (TextView)view.findViewById(R.id.tv_send);
+            view.setTag(viewvalue);
+        }
+        else{
+            view = convertView;
+            viewvalue = (ViewValue)view.getTag();
+        }
+
+        if(advisor.getType() == Advisor.TYPE_RECEIVED)
+        {
+            viewvalue.leftLayout.setVisibility(View.VISIBLE);
+            viewvalue.rightLayout.setVisibility(View.GONE);
+            viewvalue.leftAdvisor.setText(advisor.getContent());
+        }
+        else if(advisor.getType() == Advisor.TYPE_SEND)
+        {
+            viewvalue.rightLayout.setVisibility(View.VISIBLE);
+            viewvalue.leftLayout.setVisibility(View.GONE);
+            viewvalue.RightUser.setText(advisor.getContent());
+        }
 
 
         return view;
--- SEapp/app/src/main/java/com/example/zy5545269/seapp/MainActivity.java
+++ SEapp/app/src/main/java/com/example/zy5545269/seapp/MainActivity.java
@@ -2,16 +2,54 @@ package com.example.zy5545269.seapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView advisorListView;
+    private EditText inputText;
+    private Button send;
+    private AdvisorAdapter adapter;
 
+    private List<Advisor> advisorsList = new ArrayList<Advisor>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initAdvisor();
+        adapter = new AdvisorAdapter(MainActivity.this,R.layout.layout,advisorsList);
+        inputText =(EditText)findViewById(R.id.editTextInput);
+        send = (Button)findViewById(R.id.button);
+        advisorListView = (ListView)findViewById(R.id.LISTVIEW);
+        advisorListView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if(!"".equals(content))
+                {
+                    Advisor advisor =new Advisor(content,Advisor.TYPE_SEND);
+                    advisorsList.add(advisor);
+                    adapter.notifyDataSetChanged();
+                    advisorListView.setSelection(advisorsList.size());
+                    inputText.setText("");
+                }
+            }
+        });
+    }
+
+    private void initAdvisor()
+    {
+        Advisor adv1 = new Advisor("Hello, nice to meet you!", Advisor.TYPE_RECEIVED);
+        advisorsList.add(adv1);
     }
 }
